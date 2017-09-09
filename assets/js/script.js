a45db0a52250a62e77f1a08e5c8b87d2e26da62c
wilsonjuniorwj <wilsonwj0101@gmail.com>
2017-09-09 00:10:52 -0300

    Estilização da barra de navegação

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+
+function abrirFecharMenu() {
+	var btnMenu = "confinado";
+	var btn = document.getElementById("bs-example-navbar-collapse-1");
+
+	if (btnMenu === "expandir") {
+		btn.className = "navbar-collapse collapse in";
+		btnMenu = "confinado";
+	} else {
+		btn.className = "navbar-collapse collapse";
+		btnMenu = "expandir";
+	}
+}
+
+function redirecionar() {
+    $("li a").on('click', function() {
+        $("body").animate({
+            scrollTop: $($(this).attr('href')).offset().top - 30
+		}, 400);
+        abrirFecharMenu();
+        return false;
+    });
+}
