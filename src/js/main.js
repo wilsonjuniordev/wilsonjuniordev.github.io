7241eec3d252e2ec6c1c6041a6a2ba815923bd13
wilsonjuniorwj <wilsonwj0101@gmail.com>
2017-09-11 22:21:01 -0300

    Cria gitignore e inclui a paleta de cores

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,6 @@
 $(function () {
 
     scrollTo();
-    abrirFecharMenu();
     scrollBanner();
 
 });
@@ -21,24 +20,6 @@ function scrollTo() {
             }, 600)
         })
     });
-}
-
-/* =========================================
- *  abrirFecharMenu
- *  =======================================*/
-
-function abrirFecharMenu() {
-
-    var btnMenu = "confinado";
-	var btn = document.getElementById("bs-example-navbar-collapse-1");
-
-	if (btnMenu === "expandir") {
-		btn.className = "navbar-collapse collapse in";
-		btnMenu = "confinado";
-	} else {
-		btn.className = "navbar-collapse collapse";
-		btnMenu = "expandir";
-	}
 }
 
 /* =========================================
