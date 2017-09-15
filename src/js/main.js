9544e68d52fded6cfaaa4189eb74b0471dc15529
Wilson Junior <wilsonwj0101@gmail.com>
2017-09-14 23:49:25 -0300

    Mudanças no layout, espaçamentos e formulário com responsividade

--- src/js/main.js
+++ src/js/main.js
@@ -16,7 +16,7 @@ function scrollTo() {
             t.preventDefault();
             var o = $(this).attr("href");
             $("body").animate({
-                scrollTop: $(o).offset().top - 30
+                scrollTop: $(o).offset().top - 50
             }, 600)
         })
     });
